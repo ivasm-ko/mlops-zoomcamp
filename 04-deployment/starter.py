44c029b5286ddd5b435a027cee296b8a0214a3c7
Ivan Smahlenko <92439608+ivasm-ko@users.noreply.github.com>
2024-07-22 01:34:37 +0000

    module 04-deployment done

---
+++ 04-deployment/starter.py
@@ -0,0 +1,41 @@
+import sys
+import pickle
+import pandas as pd
+
+year =int(sys.argv[1])
+month = int(sys.argv[2])
+
+input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+output_file = f'output/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+
+with open('model.bin', 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+categorical = ['PULocationID', 'DOLocationID']
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+df = read_data(input_file)
+df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+
+dicts = df[categorical].to_dict(orient='records')
+X_val = dv.transform(dicts)
+y_pred = model.predict(X_val)
+
+# y_pred.std()
+y_pred.mean()
+# print(y_pred.std())
+print(y_pred.mean())
+
+if __name__ == 'main':
+    run()
